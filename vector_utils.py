7f892d0ae4b23c234621129db0d31ec325ba66fd
Nisarg08z <nisarg693z@gmail.com>
2026-05-03 11:52:47 +0530

    final

--- vector_utils.py
+++ vector_utils.py
@@ -2,7 +2,7 @@ from langchain.vectorstores import Qdrant
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema import Document
 
-QDRANT_URL = "http://44.222.24.96:6333"
+QDRANT_URL = "http://localhost:6333"
 
 def create_vector_store(chunks, collection_name):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
